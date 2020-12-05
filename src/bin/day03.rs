6ae1eb45658ea2211a8708a9c259cc3f96f532cb
Rickard Englund <rickard@r-englund.com>
2020-12-05 14:06:26 +0100

    Finsiedh day 03

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -56,10 +56,49 @@ In this example, traversing the map using this slope would cause you to encounte
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
 */
 
+/*
+--- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+*/
 
 static INPUT: &str = include_str!("day03-input.txt");
 
+fn calc_hits(right: usize, down: usize) -> u32 {
+    INPUT
+        .lines()
+        .step_by(down)
+        .enumerate()
+        .map(|(row, line)| {
+            if line.chars().nth((row * right) % line.len()).unwrap() == '#' {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
+}
 
-fn main(){
-    
+fn main() {
+    let slopes: [(usize, usize); 5] = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let res: u32 = slopes.iter().map(|(r, d)| calc_hits(*r, *d)).product();
+
+    // for (r, d) in slopes.iter() {
+    //     println!("Slope{}:{}, Trees: {}", r, d, calc_hits(*r, *d));
+    // }
+
+    println!("Product of trees hit: {}", res);
 }

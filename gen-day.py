bd6be96b79c997d0a44f13f060ff7f3e8165cc3b
Rickard Englund <rickard@r-englund.com>
2020-12-06 20:51:34 +0100

    Added script to generate files

---
+++ gen-day.py
@@ -0,0 +1,49 @@
+import sys
+import os
+
+
+
+
+if len(sys.argv) != 2: 
+    print("Usage: {} [day as a int]".format(sys.argv[0]))
+    exit()
+
+
+day = int(sys.argv[1])
+if day < 1 or day > 25:
+    print("Incorrect day, should be between 1 and 25")
+    exit()
+
+
+daystr = "day{:02}".format(day)
+rsfile = "src/bin/{}.rs".format(daystr)
+inputfile = "src/bin/{}-input.txt".format(daystr)
+
+src = """/*
+PLACEHOLDER_FOR_INSTRUCTIONS
+*/
+
+/*
+PLACEHOLDER_FOR_INSTRUCTIONS_PART_2
+*/
+
+static INPUT: &str = include_str!("{}-input.txt");
+
+fn main() {{
+
+}}
+""".format(daystr)
+
+if os.path.exists(rsfile):
+    print("File {} already exists, exiting without doing anythning".format(rsfile))
+    exit()
+
+with open(inputfile,"w") as f:
+    pass
+    
+with open(rsfile,"w") as f:
+    f.write(src)
+    
+
+
+
---
+++ src/bin/day05.rs
@@ -0,0 +1,13 @@
+/*
+PLACEHOLDER_FOR_INSTRUCTIONS
+*/
+
+/*
+PLACEHOLDER_FOR_INSTRUCTIONS_PART_2
+*/
+
+static INPUT: &str = include_str!("day05-input.txt");
+
+fn main() {
+
+}
